e1cce935ea2cef49c162cf1ac1bd90265211947b
Valentin <valentin.iclozan27@gmail.com>
2016-12-26 03:02:58 +0200

    assignment 5 updates

---
+++ newsite/week1/peer-review/app-rev2.js
@@ -0,0 +1,33 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', function ($scope) {
+    $scope.dishes = ""; 
+    $scope.message = ""; 
+    $scope.msgColor="black";
+
+    $scope.check = function () {        
+        var ndishes = $scope.dishes.trim();
+        if(ndishes.length < 1) {
+            $scope.message = "Enter data";
+            $scope.msgColor="black";
+        }
+        else {
+            ndishes = ndishes.split(',');    
+            if(ndishes.length <= 3) {
+                $scope.message = "Enjoy!";
+                $scope.msgColor="green";
+            }            
+            else if(ndishes.length > 3) {
+                $scope.message = "Too much!";
+                $scope.msgColor = "red";
+            }
+        }  
+
+		console.log(ndishes.length);
+    };
+});
+
+
+})();
